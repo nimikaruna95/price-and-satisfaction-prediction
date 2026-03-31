fcce8a07d04e83b97dd5b7a590bfac8ca6cfb4e1
nimikaruna95 <165786611+nimikaruna95@users.noreply.github.com>
2026-03-31 20:04:29 +0530

    Update satisfaction_preprocessing.py

--- preprocessing/satisfaction_preprocessing.py
+++ preprocessing/satisfaction_preprocessing.py
@@ -1,37 +1,33 @@
 # satisfaction_preprocessing.py
 import pandas as pd
+
 def preprocess_satisfaction_data(input_path, output_path):
 
     df = pd.read_csv(input_path)
     print("Original Shape:", df.shape)
 
     df.drop_duplicates(inplace=True)
-    df["Arrival Delay in Minutes"] = df["Arrival Delay in Minutes"].fillna(0)
 
-    # Fix of the target
-    df["satisfaction"] = df["satisfaction"].str.strip().str.lower()
+    # Fill missing delay
+    df["Arrival Delay in Minutes"].fillna(0, inplace=True)
 
-    # Fixing Column Names
+    # Clean column names
     df.columns = df.columns.str.strip()
 
-    print("Original Columns:", df.columns.tolist())
+    # Clean target
+    df["satisfaction"] = df["satisfaction"].str.strip().str.lower()
 
-    # Removing the index column which is first column
-    if df.columns[0] in ["", "Unnamed: 0"] or df.iloc[:, 0].equals(pd.Series(range(len(df)))):
-        df.drop(df.columns[0], axis=1, inplace=True)
-        print("Dropped first index column")
-
-    # Remove Columns
     drop_cols = []
 
     for col in df.columns:
-        if "unnamed" in col.lower() or col.strip() in ["id", ",id", ""]:
+        if "unnamed" in col.lower() or col.lower() == "id":
             drop_cols.append(col)
 
     df.drop(columns=drop_cols, inplace=True, errors="ignore")
 
     print("Dropped columns:", drop_cols)
 
+    # Service Score
     service_columns = [
         "Inflight wifi service",
         "Departure/Arrival time convenient",
@@ -48,20 +44,27 @@ def preprocess_satisfaction_data(input_path, output_path):
         "Inflight service",
         "Cleanliness"
     ]
-    
+
     df["Total_Service_Score"] = df[service_columns].sum(axis=1)
 
-    # Age group feature
+    # Delay Feature 
+    df["Total_Delay"] = (
+        df["Departure Delay in Minutes"] +
+        df["Arrival Delay in Minutes"]
+    )
+
+    # Age Group
     df["Age_Group"] = pd.cut(
         df["Age"],
         bins=[0, 18, 30, 45, 60, 100],
         labels=["Teen", "Young Adult", "Adult", "Senior", "Elder"]
-    )  
-    #df.columns = df.columns.str.strip()
+    )
+
     df.to_csv(output_path, index=False)
-    print("customer cleaned csv file is saved.")
+
+    print("Customer cleaned csv file saved ")
+    print("Final Shape:", df.shape)
 
 if __name__ == "__main__":
-    preprocess_satisfaction_data("data/Passenger_Satisfaction.csv", "data/passenger_cleaned.csv")
-    print("Satisfaction preprocessing completed successfully.")
-
+    preprocess_satisfaction_data( "data/Passenger_Satisfaction.csv", "data/passenger_cleaned.csv"    )
+    print("Satisfaction preprocessing completed successfully ")
